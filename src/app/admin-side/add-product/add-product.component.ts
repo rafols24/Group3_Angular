656e7d64c90e16fb3c555ee477bbbe0f4162d8cf
Ann143 <mtelez138@gmail.com>
2021-08-03 14:38:35 +0800

    Admin

--- src/app/admin-side/add-product/add-product.component.ts
+++ src/app/admin-side/add-product/add-product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-product',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
 
-  constructor() { }
+
+  product: any = FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.product = this.formBuilder.group({
+      product_name: ['', [Validators.required, Validators.minLength(2)]],
+      price: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(15)]],
+      product_image: ['', [Validators.required]],
+    });
   }
+
+dataOfProduct: any;
+
+url  = "http://127.0.0.1:8000/api/product/create";
+
+onSubmit(){
+  this.submitted = true;
+
+  if (this.product.invalid) {
+    return;
+  }
+
+  console.warn(this.product.value);
+
+  this.http.post(this.url, this.product.value).subscribe(productData =>{
+  this.dataOfProduct = productData;
+
+  
+  Swal.fire({
+    text: 'You added a product Successfully!',
+    icon: 'success'
+  });
+
+  console.log(productData);
+  this.router.navigate(['/addproduct']);
+  },
+   errors =>{
+    Swal.fire('Oops...', 'Something went wrong!', 'error')
+    console.log(errors.error.errors);
+   }
+  );
+}
+
 
 }
