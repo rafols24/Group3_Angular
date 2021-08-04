d8e37db4dc4f74605765b9f46fd60d0de725fa6c
lesh24 <leshleereyes@gmail.com>
2021-08-04 09:06:47 +0800

    partial2

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -23,50 +23,50 @@ export class ProductListComponent implements OnInit {
   {
     name: "Kyrie Low",
     price: 5500,
-    description: "This is the nike shoes"
+    description: "This is an amazing Kyrie Edition Nice Shoes 100% durable"
 
 
   },
   {
     name: "Lebron Soldier",
     price: 6500,
-    description: "This is the nike shoes"
+    description: "This shoes is one of lebrons amazing nike collection better grab it now."
 
   },
   {
     name: "Lebron Witness IV ",
     price: 5000,
-    description: "This is the nike shoes"
+    description: "Amazing Lebron Witness Fourth edition nike shoes, limited only."
 
   },
   {
-    name: "Lebron Witness V-White",
+    name: "Lebron Witness V",
     price: 6000,
-    description: "This is the nike shoes"
+    description: "A more durable and amazing collection of nike Lebron Edition"
 
   },
    {
     name: "Lebron VII Grey",
     price: 10000,
-    description: "This is the nike shoes"
+    description: "This shoes makes it elegant to look due to its grey color"
 
   },
   {
     name: "Lebron VII PS",
     price: 7250,
-    description: "This is the nike shoes"
+    description: "You can't miss out Lebrons famous Nike Shoes seventh edition"
 
   },
   {
     name: "Ultraboost",
     price: 6000,
-    description: "This is the nike shoes"
+    description: "This Ultraboost nike shoes it one of the top trends for athletes "
 
   },
   {
     name: "Nike Shirt",
     price: 500,
-    description: "This is the nike shoes"
+    description: "You should not misseed the limited editionof this nike shirt"
 
   }
 
